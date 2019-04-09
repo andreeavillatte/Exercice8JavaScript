680ec186ad0de066e6d9b7951daad2520171b916
core.editor <andreeavillatte@gmail.com>
2019-04-09 14:04:30 +0200

    ajout verification chiffres ou text

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function getAge() {
-    if (age.value >= 18) {
-        alert ("Vous êtes majeur");
-    } else {
-        alert ("Vous êtes mineur");
+    if (isNaN(age.value) || isNaN(age.value)){
+        alert("Merci de n'entrer que des ciffres...");
+    } else{
+        if (age.value >= 18) {
+            alert ("Vous êtes majeur");
+        } else {
+            alert ("Vous êtes mineur");
+        }
     }
 }
